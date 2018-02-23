7e041f649658fd1d64b19a7e6021fca08838e1b3
skbrown333 <skbrown333@gmail.com>
2018-02-23 11:28:01 -0600

    fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es6": true
+        "es6": true,
+        "node": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
@@ -10,7 +10,7 @@ module.exports = {
     "rules": {
         "indent": [
             "error",
-            "tab"
+            4
         ],
         "linebreak-style": [
             "error",
--- src/analytics.js
+++ src/analytics.js
@@ -23,42 +23,42 @@ const HUBOT_SITE_NAME = process.env.HUBOT_SITE_NAME;
 const HUBOT_GOOGLE_ANALYTICS_ID = process.env.HUBOT_GOOGLE_ANALYTICS_ID;
 
 module.exports = function(robot) {
-	robot.respond(/users/, getAnalytics);
+    robot.respond(/users/, getAnalytics);
 };
 
 function getAnalytics (res) {
-	const gapi = new GAPI({
-		iss: serviceEmail,
-		scope: 'https://www.googleapis.com/auth/analytics.readonly',
-		keyFile: keyFilePath,
-	}, function(err) {
+    const gapi = new GAPI({
+        iss: serviceEmail,
+        scope: 'https://www.googleapis.com/auth/analytics.readonly',
+        keyFile: keyFilePath,
+    }, function(err) {
         
         gapi.getToken(function(err, token) { 
             let util = require('util');
-			let config = {
-				'token': token,
-			};
-			let ga = new GA.GA(config);
+            let config = {
+                'token': token,
+            };
+            let ga = new GA.GA(config);
 
             
-			const metrics = [
-				'rt:activeUsers',
-			];
+            const metrics = [
+                'rt:activeUsers',
+            ];
 
-			const options = {
-				'ids': `ga:${ HUBOT_GOOGLE_ANALYTICS_ID }`,
-				'metrics': metrics.join(','),
-			};
+            const options = {
+                'ids': `ga:${ HUBOT_GOOGLE_ANALYTICS_ID }`,
+                'metrics': metrics.join(','),
+            };
 
 
-			ga.get(options, function(err, entries) {
-				if(!entries || !entries[0]) {
-					res.reply(`${ HUBOT_SITE_NAME } currently has 0 active users`);
-					return;
-				}
-				res.reply(`${ HUBOT_SITE_NAME } currently has ${entries[0].metrics[0]['rt:activeUsers']} active users`);
-			});
+            ga.get(options, function(err, entries) {
+                if(!entries || !entries[0]) {
+                    res.reply(`${ HUBOT_SITE_NAME } currently has 0 active users`);
+                    return;
+                }
+                res.reply(`${ HUBOT_SITE_NAME } currently has ${entries[0].metrics[0]['rt:activeUsers']} active users`);
+            });
 
-		});     
-	});
+        });     
+    });
 }
